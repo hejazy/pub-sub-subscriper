c513be0cd7c40a2d5c13e59b4eace9a8a54939cd
mohsen hegazy <hegazy@outlook.com>
2021-08-02 16:05:40 +0200

    UPD: adding implementation

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const  {PubSub} = require('@google-cloud/pubsub');
 
 const init = async ()=>{
+    const topicName = process.env.TOPIC || 'topic-name';
+    const subscriptionName= process.env.SUBSCRIPTION || 'subscription-name'
 
+    pubsub = new PubSub({projectId: process.env.PROJECT_ID});
+    topic = await pubsub.topic(topicName);
+    
+
+    const subscription = topic.subscription(subscriptionName);
+    console.log('--------------------------------------------')
+    console.log(`start listening on topic: ${topicName}, subscription: ${subscriptionName}`)
+    console.log('--------------------------------------------')
+    subscription.on('message',(msg)=>{
+        const content = JSON.parse(msg.data.toString());
+        msg.ack()
+        console.log(content);
+    });
 }
 
 
